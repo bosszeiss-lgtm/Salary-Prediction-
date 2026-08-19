376aec286c0ffd02b947d5e622627c3730e0e2c2
bosszeiss-lgtm <bosszeiss@gmail.com>
2026-08-19 10:55:31 +0530

    Updated age and salary prediction

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -18,11 +18,11 @@ encoder = joblib.load("Label_Encoder.pkl")
 
 st.title("Salary Prediction")
 
-age = st.number_input("Age")
+age = st.number_input("Age",18,65)
 gender = st.selectbox("Gender",encoder["Gender"].classes_)
 education_level = st.selectbox("Education Level",encoder["Education Level"].classes_)
 job_title = st.selectbox("Job Title",encoder["Job Title"].classes_)
-years_of_experience = st.number_input("Years of Experience")
+years_of_experience = st.number_input("Years of Experience",0,40)
 
 df = pd.DataFrame({
     "Age":[age],
@@ -34,7 +34,7 @@ df = pd.DataFrame({
 
 if st.button("Predict Salary"):
   for col in encoder:
-    df(col) = encoder[col].transform(df[col])
+    df[col] = encoder[col].transform(df[col])
 
 prediction = model.predict(df)
 st.success(f"Predicted Salary: {prediction[0].2f}")
